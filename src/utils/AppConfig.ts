15d46a3b665eb8090950b408120ad96eab33603e
zafs23 <sajiazafreen@u.boisestate.edu>
2025-02-15 10:22:42 -0800

    update site name

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
+  site_name: 'OneCode Tech Interview',
+  title: 'OneCode Tech Interview',
   description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  author: 'Sajia Zafreen',
   locale_region: 'en-us',
   locale: 'en',
 };
